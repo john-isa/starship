544242b137f7dd7664a749c80e95622b72c4ed9a
John Isailofski <john.isailofski@gmail.com>
2022-02-27 15:46:29 +1100

    Created development integration project

--- internal/starships/db_starship.go
+++ internal/starships/db_starship.go
@@ -1,140 +1,157 @@
 package starships
 
 import (
+	"errors"
+	"fmt"
 	"starships/internal/starships/models"
-
-	"github.com/palantir/stacktrace"
 )
 
-// UserService will hold the connection and key db info
+// StarshipService will hold the connection and key db info.
 type StarshipService struct {
-	StarshipList  map[int]models.Starship
-	MaxStarshipID int
+	StarshipList []models.Starship
 }
 
-// NewUserService creates a new Carer Service with the system's database connection
-func NewStarshipService(list map[int]models.Starship, count int) *StarshipService {
+// CreateStarshipService creates a new Starship Management Service with the system's database connection.
+func CreateStarshipService(list []models.Starship) *StarshipService {
 	return &StarshipService{
-		StarshipList:  list,
-		MaxStarshipID: count,
+		StarshipList: list,
 	}
 }
 
+func (service *StarshipService) ListAllStarships() ([]models.Starship, error) {
+	shipList := service.StarshipList
+	if len(shipList) == 0 {
+		return shipList, errors.New("no starships in the inventory")
+	}
+	return shipList, nil
+}
+
+// ListStarships creates a list of sharships defined by a filter
 func (service *StarshipService) ListStarships(filterBy string, value string) ([]models.Starship, error) {
 	shipList, err := service.ListAllStarships()
+	if err != nil {
+		return []models.Starship{}, err
+	}
 	list := []models.Starship{}
 	switch filterBy {
 	case "none":
 		return shipList, err
 
 	case "name":
-		for v := 0; v < len(shipList); v++ {
-			if shipList[v].Name == value {
-				list = append(list, shipList[v])
+		for _, ship := range shipList {
+			if ship.Name == value {
+				list = append(list, ship)
 			}
 		}
 	case "class":
-		for v := 0; v < len(shipList); v++ {
-			if shipList[v].Class == value {
-				list = append(list, shipList[v])
+		for _, ship := range shipList {
+			if ship.Class == value {
+				list = append(list, ship)
 			}
 		}
 	case "status":
-		for v := 0; v < len(shipList); v++ {
-			if shipList[v].Status == value {
-				list = append(list, shipList[v])
+		for _, ship := range shipList {
+			if ship.Status == value {
+				list = append(list, ship)
 			}
 		}
+	}
+	if len(list) == 0 {
+		str := fmt.Sprintf("no ship are available with the filter of type: %s and value %s", filterBy, value)
+
+		return []models.Starship{}, errors.New(str)
 	}
 	return list, nil
 }
 
-func (service *StarshipService) ListAllStarships() ([]models.Starship, error) {
-	// TBD
-	return []models.Starship{}, nil
-}
+// GetStarship gets the selected starship from the inventory
+func (service *StarshipService) GetStarship(i int) (models.Starship, error) {
+	ships := []models.Starship{}
 
-// GetUser returns a single JSON document
-func (service *StarshipService) GetStarships(i int) (models.Starship, error) {
-	user, ok := service.StarshipList[i]
-	if !ok {
-		return models.Starship{}, stacktrace.NewError("Failure trying to retrieve user")
+	for _, ship := range service.StarshipList {
+		if ship.ID == i {
+			ships = append(ships, ship)
+		}
 	}
-	return user, nil
+
+	if len(ships) == 0 {
+		return models.Starship{}, errors.New("no ship entry found")
+	}
+
+	if len(ships) > 1 {
+		return models.Starship{}, errors.New("error: ship ID entries")
+	}
+
+	return ships[0], nil
 }
 
-// AddUser adds a User JSON document, returns the JSON document with the generated id
-func (service *StarshipService) NewStarship(u models.Starship) (models.Starship, error) {
-	service.MaxStarshipID = service.MaxStarshipID + 1
-	u.ID = service.MaxStarshipID
-	service.StarshipList[service.MaxStarshipID] = u
+// CreateStarship creates and adds a starship to the inventory
+func (service *StarshipService) CreateStarship(u models.Starship) (models.Starship, error) {
+	service.StarshipList = append(service.StarshipList, u)
+
 	return u, nil
 }
 
-// UpdateUser updates an existing user
-func (service *StarshipService) UpdateStarship(u models.Starship) (models.Starship, error) {
-	id := u.ID
-	_, ok := service.StarshipList[id]
-	if !ok {
-		return u, stacktrace.NewError("Failure trying to update user")
-	}
-	service.StarshipList[id] = u
-	return service.StarshipList[id], nil
+// UpdateStarship updates an existing starship entry in the inventory
+func (service *StarshipService) UpdateStarship(ship models.Starship) (models.Starship, error) {
+	service.StarshipList[ship.ID] = ship
+
+	return service.StarshipList[ship.ID], nil
 }
 
-// DeleteUser deletes a user
+// DeleteStarship deletes a starship from the inventory
 func (service *StarshipService) DeleteStarship(i int) error {
-	_, ok := service.StarshipList[i]
-	if !ok {
-		return stacktrace.NewError("Failure trying to delete user")
-	}
-	delete(service.StarshipList, i)
+	ship := service.StarshipList[i]
+
+	service.StarshipList = append(service.StarshipList[:ship.ID], service.StarshipList[ship.ID+1])
+
 	return nil
 }
 
 // CreateMockDataSet initialises a database for test purposes. It returns a list of User objects
 // as well as the new max object count
-func CreateMockDataSet() (map[int]models.Starship, int) {
-	list := make(map[int]models.Starship)
-	list[0] = models.Starship{
-		ID:    0,
-		Name:  "Lorikeet",
-		Class: "Scout",
-		Armament: []models.Armament{
-			{
-				ID:       0,
-				ShipId:   0,
-				Title:    "Extended Range Sensor Package",
-				Quantity: "500",
+func CreateMockDataSet() []models.Starship {
+	list := []models.Starship{
+		{
+			ID:    0,
+			Name:  "Lorikeet",
+			Class: "Scout",
+			Armament: []models.Armament{
+				{
+					ID:       0,
+					ShipId:   0,
+					Title:    "Extended Range Sensor Package",
+					Quantity: "60",
+				},
 			},
+			Crew:   25,
+			Image:  "https://no.image.yet",
+			Value:  350000000.00,
+			Status: "ACTIVE",
 		},
-		Crew:   25,
-		Image:  "https://no.image.yet",
-		Value:  350000000.00,
-		Status: "ACTIVE",
-	}
-	list[1] = models.Starship{
-		ID:    1,
-		Name:  "Obliterator",
-		Class: "Scout",
-		Armament: []models.Armament{
-			{
-				ID:       0,
-				ShipId:   1,
-				Title:    "Ion Cannon",
-				Quantity: "500",
+		{
+			ID:    1,
+			Name:  "Obliterator",
+			Class: "Scout",
+			Armament: []models.Armament{
+				{
+					ID:       0,
+					ShipId:   1,
+					Title:    "Ion Cannon",
+					Quantity: "500",
+				},
+				{
+					ID:       1,
+					ShipId:   1,
+					Title:    "Ion Cannon",
+					Quantity: "500",
+				},
 			},
-			{
-				ID:       1,
-				ShipId:   1,
-				Title:    "Ion Cannon",
-				Quantity: "500",
-			},
+			Crew:   5472910,
+			Image:  "https://no.image.yet",
+			Value:  600000000000.00,
+			Status: "ACTIVE",
 		},
-		Crew:   5472910,
-		Image:  "https://no.image.yet",
-		Value:  600000000000.00,
-		Status: "ACTIVE",
 	}
-	return list, len(list) - 1
+	return list
 }
